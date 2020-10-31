8f564eeddb175439b35dbe38de5bb1a1260b051c
wainfotechnologies <72775989+wainfotechnologies@users.noreply.github.com>
2020-10-31 18:54:44 +0530

    d

--- _content/Blazored.Localisation/blazored-localisation.js
+++
@@ -1,8 +0,0 @@
-window.blazoredLocalisation = {
-    getBrowserLocale: function() {
-        return navigator.languages && navigator.languages.length ? navigator.languages[0] : navigator['userLanguage']
-            || navigator.language
-            || navigator['browserLanguage']
-            || 'en';
-    }
-};
